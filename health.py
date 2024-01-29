a8774f4c917762dd8f322e3915f67049dd72ad12
Arjun.K <37630273+arj0505@users.noreply.github.com>
2024-01-29 15:55:51 +0530

    Add files via upload
    updated the code.

--- health.py
+++ health.py
@@ -1,20 +1,17 @@
-### Health Management APP
 from dotenv import load_dotenv
-
-load_dotenv() ## load all the environment variables
-
 import streamlit as st
 import os
 import google.generativeai as genai
 from PIL import Image
 
+load_dotenv()  # load all the environment variables
+
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-## Function to load Google Gemini Pro Vision API And get response
-
-def get_gemini_repsonse(input,image,prompt):
-    model=genai.GenerativeModel('gemini-pro-vision')
-    response=model.generate_content([input,image[0],prompt])
+# Function to load Google Gemini Pro Vision API And get response
+def get_gemini_response(input, image, prompt):
+    model = genai.GenerativeModel('gemini-pro-vision')
+    response = model.generate_content([input, image[0], prompt])
     return response.text
 
 def input_image_setup(uploaded_file):
@@ -32,44 +29,41 @@ def input_image_setup(uploaded_file):
         return image_parts
     else:
         raise FileNotFoundError("No file uploaded")
-    
-##initialize our streamlit app
 
+# initialize our streamlit app
 st.set_page_config(page_title="Gemini Health App")
-
 st.header("Gemini Health App")
-input=st.text_input("Input Prompt: ",key="input")
+
+input_prompt = """
+    You are an expert in nutritionist where you need to see the food items from the image
+    and calculate the total calories, also provide the details of every food items with calories intake
+    in the below format:
+
+    1. Item 1 - no of calories
+    2. Item 2 - no of calories
+    ----
+    ----
+    
+    Finally, you can also mention whether the food is healthy or not and also
+    mention the percentage split of the ratio of carbohydrates, fats, fibers, sugar, and other important
+    things required in our diet.
+"""
+
+input = st.text_input("Input Prompt: ", key="input")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-image=""   
+image = ""
+
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image.", use_column_width=True)
 
-
-submit=st.button("Tell me the total calories")
-
-input_prompt="""
-You are an expert in nutritionist where you need to see the food items from the image
-               and calculate the total calories, also provide the details of every food items with calories intake
-               is below format
-
-               1. Item 1 - no of calories
-               2. Item 2 - no of calories
-               ----
-               ----
-        Finally you can also mention whether the food is healthy or not and also
-        mentioned the percentage split of the ration of carbohydrates, fats, fibers, sugar and other important
-        things required on our diet.       
-
-
-"""
-
-## If submit button is clicked
+submit = st.button("Tell me the total calories")
 
 if submit:
-    image_data=input_image_setup(uploaded_file)
-    response=get_gemini_repsonse(input_prompt,image_data,input)
-    st.subheader("The Response is")
-    st.write(response)
-
-
+    if uploaded_file is None:
+        st.warning("Please upload the image.")
+    else:
+        image_data = input_image_setup(uploaded_file)
+        response = get_gemini_response(input_prompt, image_data, input)
+        st.subheader("The Response is")
+        st.write(response)
